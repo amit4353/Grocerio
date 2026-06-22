ee73a503db7a949f5c5b7de6e9d71da1ddceacc9
Amit <amitprajapati4353@gmail.com>
2026-06-22 19:35:52 +0530

    fix render dynamic port configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const db = require("./config/db.js");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
-const port = 3000;
 require("dotenv").config();
 
 app.use(cors({
@@ -11,17 +9,14 @@ app.use(cors({
     credentials : true
 }));
 
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-
 
 const userRoute = require("./routes/userRoute.js");
 const productRoute = require("./routes/productRoute.js");
 const cartRoute = require("./routes/cartRoute.js");
 const orderRoute = require("./routes/orderRoute.js");
-
 
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
@@ -31,6 +26,8 @@ app.use("/api/orders", orderRoute);
 app.get("/",(req,res) => {
     res.send("Backend Grocerio Home Page");
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port,() => {
     console.log("Server is running on Port : " , port);
